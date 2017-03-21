63eefe5a055eef086ce169b810a0ac1c841cc572
kavita <kavita@DESKTOP-7CHTLON>
2017-03-21 19:39:35 +0530

    ManageVisitie21March

---
+++ WebApiForManageVisitors/Models/DepartmentMaster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiForManageVisitors.Models
+{
+   [ MetadataType(typeof(DepartmentMaster))]
+    public partial class tbl_DepartmentMaster
+    {
+    }
+    public class DepartmentMaster
+    {
+        [Display(Name = "Department Name")]
+        public string DepartmentName { get; set; }
+
+        [Display(Name = "Department ID")]
+        public string DepartmentID { get; set; }
+    }
+}
